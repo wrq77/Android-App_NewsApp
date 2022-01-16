7ccc935af3203cc9603ac6561c780bae8c1cae2e
yejiyuan <1286412801@qq.com>
2022-01-16 17:48:18 +0100

    update user

--- app/src/main/java/fr/isep/news/Model/User.java
+++ app/src/main/java/fr/isep/news/Model/User.java
@@ -10,12 +10,6 @@ public class User {
     private String email;
     private String password;
 
-    public User(String userName, String email, String password) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-    }
-
     public User(String userName, String email) {
         this.userName = userName;
         this.email = email;
